52dc8f129685695af850c50e6a88c95b9fdcedca
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-07-19 16:54:44 +0900

    無駄コードの削除

--- make_network_data.py
+++ make_network_data.py
@@ -38,15 +38,8 @@ def partition_graph(G):
 
 def draw_spring_layout(G, partition):
     pos = nx.spring_layout(G)
-    size = len(set(partition.values()))
     random.seed(0)
-    start_color = (51, 255, 0)
-    end_color = (0, 0, 255)
-    r_map = np.linspace(start_color[0], end_color[0], num=size)
-    g_map = np.linspace(start_color[1], end_color[1], num=size)
-    b_map = np.linspace(start_color[2], end_color[2], num=size)
     for index, com in enumerate(set(partition.values())):
-        color_tupple = (r_map[index], g_map[index], b_map[index])
         list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]
         nx.draw_networkx_nodes(G, pos, list_nodes, node_size=1,
                                node_color='#%02x%02x%02x' % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
@@ -63,7 +56,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(type(result), dict)
 
 if __name__ == '__main__':
-    G = nx.from_scipy_sparse_matrix(make_matrix_from_tsv_file("data/30_sample_pairs.tsv")[1])
+    G = nx.from_scipy_sparse_matrix(make_matrix_from_tsv_file("data/selected_pairs.tsv")[1])
     partition = partition_graph(G)
     draw_spring_layout(G, partition)
     unittest.main()
